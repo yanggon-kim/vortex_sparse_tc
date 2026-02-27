vortexgpgpu/vortex

new instruction working mma_struct_sparse_sync by claude code

hw/rtl/core/VX_uop_sequencer.sv
@@ -39,7 +39,9 @@ module VX_uop_sequencer import
 
 `ifdef EXT_TCU_ENABLE
 
-    assign is_uop_input = (input_if.data.ex_type == EX_TCU && input_if.data.op_type == INST_TCU_WMMA);
+    assign is_uop_input = (input_if.data.ex_type == EX_TCU
+        && (input_if.data.op_type == INST_TCU_WMMA
+         || input_if.data.op_type == INST_TCU_WMMA_SP));
 
     VX_tcu_uops tcu_uops (
         .clk     (clk),

yanggon-kim/vortex_sparse_tc

hw/rtl/core/VX_uop_sequencer.sv
@@ -45,7 +45,9 @@ module VX_uop_sequencer import
 
 `ifdef EXT_TCU_ENABLE
 
-    assign is_base_uop_input = (input_if.data.ex_type == EX_TCU && input_if.data.op_type == INST_TCU_WMMA);
+    assign is_base_uop_input = (input_if.data.ex_type == EX_TCU
+        && (input_if.data.op_type == INST_TCU_WMMA
+         || input_if.data.op_type == INST_TCU_WMMA_SP));
 
     VX_tcu_uops tcu_uops (
         .clk     (clk),